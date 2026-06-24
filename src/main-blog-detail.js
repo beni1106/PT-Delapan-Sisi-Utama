f1d928686bbdead8455d9ef45d229586c8bb66e1
Revan Novendri <revannofendri@gmail.com>
2026-06-24 10:14:36 +0700

    solve problem

--- src/main-blog-detail.js
+++ src/main-blog-detail.js
@@ -6,12 +6,5 @@
 import './styles/main.css';
 import { initBlogDetailPage } from './controllers/blogDetail.controller.js';
 
-document.addEventListener('DOMContentLoaded', initBlogDetailPage);/**
- * main-blog-detail.js
- * Entry point untuk blog-detail.html (halaman detail artikel).
- */
-
-import './styles/main.css';
-import { initBlogDetailPage } from './controllers/blogDetail.controller.js';
-
 document.addEventListener('DOMContentLoaded', initBlogDetailPage);
+
